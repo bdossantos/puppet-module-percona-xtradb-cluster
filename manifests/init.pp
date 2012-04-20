d4956789a2d3787f6587ae018e7d947d18ab9237
Benjamin Dos Santos <benj.ds@gmail.com>
2012-04-21 00:52:44 +0200

    manage repository url, mysql server id and join cluster IP address

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,9 @@ class percona_xtradb_cluster(
     $keyserver  = 'hkp://keys.gnupg.net',
     $recv_keys  = '1C4CBDCDCD2EFD2A',
     $gpg_export = 'CD2EFD2A',
-    $repository = 'http://repo.percona.com/apt'
+    $repository = 'http://repo.percona.com/apt',
+    $mysql_serverid = 1,
+    $joinip = ''
     ) {
 
     include percona_xtradb_cluster::packages,
